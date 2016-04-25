a8e7714b5a66e4dab6db7fee686d2e6f227ceffb
Andrew Smith-Mui <arsm800@gmail.com>
2016-04-25 11:04:10 -0400

    Created nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   root to: "states#index"
 
+  resources :states do
+  resources :stations
+end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
